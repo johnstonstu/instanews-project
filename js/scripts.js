427618ecf5d547d5115f923500cec4c8e72d8ee0
johnstonstu <stu.w.johnston@gmail.com>
2018-04-23 14:41:08 -0700

    selector applies correct api request

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 $(function() {
-  // Built by LucyBot. www.lucybot.com
-  var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-  url +=
-    "?" +
-    $.param({
-      "api-key": "1dcc8ad4ce2442b19fc5355e2305e259"
-    });
-  $.ajax({ url: url, method: "GET" })
-    .done(function(result) {
-      console.log(result);
-    })
-    .fail(function(err) {
-      throw err;
-    });
+  $("select").on("change", function() {
+    var selected = $(this).val();
+
+    console.log(selected);
+
+    // Built by LucyBot. www.lucybot.com
+    var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+    url +=
+      "?" +
+      $.param({
+        "api-key": "1dcc8ad4ce2442b19fc5355e2305e259"
+      });
+
+    $.ajax({ url: url, method: "GET" })
+      .done(function(result) {
+        console.log(result);
+
+        // console.log(response);
+      })
+
+      .fail(function(err) {
+        //   api request error
+        throw err;
+      })
+      .always(function() {
+        console.log("always runs");
+      });
+  });
 });
