a4c02de7c6ad3f76763af704e40f24e0ac1fd95e
johnstonstu <stu.w.johnston@gmail.com>
2018-04-30 12:06:51 -0700

    added selectric, changed header and footer active state scaling

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(function() {
-  $("select").on("change", function() {
-    // saves selected value from selector drop-down
+  // initalizes selectric selector
+  $("select").selectric();
 
+  // when the dropdown selector changes, runs the following scripts
+  $("select").on("change", function() {
     // active state for resizing header
     $("header").addClass("header--active");
+    $("footer").addClass("footer--active");
 
     //clears previous list
     $(".stories").empty();
@@ -14,6 +17,7 @@ $(function() {
       '<img alt="loader" src="../images/ajax-loader.gif" width="100" height="100" align="center" />'
     );
 
+    // saves selected value from selector drop-down
     var selected = $(this).val();
 
     //api get requeset
@@ -25,7 +29,12 @@ $(function() {
         "api-key": "1dcc8ad4ce2442b19fc5355e2305e259"
       });
 
-    $.ajax({ url: url, method: "GET" })
+    $.ajax({
+      url: url,
+      method: "GET"
+    })
+
+      // runs after a succesful api request
       .done(function(data) {
         var arr = data.results;
 
@@ -37,25 +46,30 @@ $(function() {
           .slice(0, 12);
 
         //loops through results array
-        for (var i = 0; i <= pasredArr.length; i++) {
-          var abs = pasredArr[i].abstract;
-          var imgURL = pasredArr[i].multimedia[4].url;
-          var storyURL = pasredArr[i].url;
+        if (pasredArr.length >= 12) {
+          for (var i = 0; i <= pasredArr.length; i++) {
+            var abs = pasredArr[i].abstract;
+            var imgURL = pasredArr[i].multimedia[4].url;
+            var storyURL = pasredArr[i].url;
 
-          // adds stories to the page with link, picture and abstract
-          $(".stories").append(
-            '<li> <a href="' +
-              storyURL +
-              '"> <div class="imgBG" style="background-image: url(' +
-              imgURL +
-              ')"><p>' +
-              abs +
-              "</p></div></a></li>"
-          );
-          // clears the loading gif
+            // adds stories to the page with link, picture and abstract
+            $(".stories").append(
+              '<li> <a href="' +
+                storyURL +
+                '"> <div class="imgBG" style="background-image: url(' +
+                imgURL +
+                ')"><p>' +
+                abs +
+                "</p></div></a></li>"
+            );
+            // clears the loading gif
+            $(".loading").empty();
+          }
+        } else {
+          alert("Not enough stories to display.");
           $(".loading").empty();
         }
-      })
+      }) // end of .done
 
       // throws error if api cannot be reached
       .fail(function(err) {
@@ -66,5 +80,5 @@ $(function() {
       .always(function() {
         $(".loading").empty();
       });
-  });
+  }); // end of .on "change" function
 });
